167e82cf9dcad32d6fb3aebcb0d9779bbe1c7566
NazarSadovij <sadovijnazar52@gmail.com>
2025-06-10 21:05:31 +0300

    new file

--- BRON_PROJECT/views.py
+++ BRON_PROJECT/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from booking.models import Room, Booking
+
 # Create your views here.
 def main_page(request):
+    rooms = Room.objects.all()
 
     context = {
-        "data": "Привіт з джанго"
+        "data": "Привіт з джанго",
+        'room_list': rooms
     }
 
-    return render(request, "index.html", context)
+    return render(request, "BRON_PROJECT/room_list.html", context)
