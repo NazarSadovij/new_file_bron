c52b175a47966436b5b5733a90b3ce3619f62a59
NazarSadovij <sadovijnazar52@gmail.com>
2025-06-17 21:06:15 +0300

    new file...

--- BRON/urls.py
+++ BRON/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from BRON_PROJECT import views
+from django.conf.urls.static import static
+from . import settings
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.main_page),
-]
+
+    path('admin/', admin.site.urls),
+    path('', include('booking.urls')),
+    path('', include('users_sustem.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- templates/users_system/urls.py
+++ templates/users_system/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from BRON_PROJECT import views
 
 urlpatterns = [
-    path('', views.main_page),
+    path()
 ]
